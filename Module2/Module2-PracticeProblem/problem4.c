ff31f81cd6eac699f2f108281f3e57db7278a7cf
Md. Selim Hossain <mrrafi812345@gmail.com>
2023-04-03 22:04:44 +0600

    fourth problem solvev

---
+++ Module2/Module2-PracticeProblem/problem4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int num;
+    scanf("%d", &num);
+    
+    if(num == 0)
+    {
+        printf("zero");
+    }
+    else if(num >=1)
+    {
+        printf("positive");
+    }
+    else{
+        printf("negative");
+    }
+}
