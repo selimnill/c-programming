ed9bfb5d85448b97ca38a15e73082809a8d35fcf
Md. Selim Hossain <mrrafi812345@gmail.com>
2023-04-03 21:43:49 +0600

    first problem solved

--- Module2/Arithmetic.c
+++ Module2/Arithmetic.c
@@ -1,9 +1,25 @@
-#include<stdio.h>
+// #include<stdio.h>
 
-int main()
-{
-    int a=8, b=20;
-    float sum = b*1.0/a;
-    printf("%.1f\n",sum);
-    return 0;
-}
+// int main()
+// {
+//     int a=8, b=20;
+//     float sum = b*1.0/a;
+//     printf("%.1f\n",sum);
+//     return 0;
+// }
+ #include <stdio.h>
+    int main()
+    {
+        int x = 0;
+        if (x == 1) {
+            if (x >= 0) {
+                printf("true ");
+            }
+            else {
+                printf("false");
+            }
+        }
+        else{
+            printf("hsdf");
+        }
+    }
